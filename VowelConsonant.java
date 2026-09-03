8f1a1c40ec1b7e0eeb8d0c6b22198d769b2f8a05
Pruthvi Patil <pruthvipatil451@gmail.com>
2026-09-03 17:19:10 +0530

    Add files via upload

---
+++ VowelConsonant.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class VowelConsonant
+{
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+
+System.out.println("enter a character");
+char ch = sc.next().charAt(0);
+
+if (ch == 'a' || ch =='e' || ch == 'i' || ch == 'o' || ch == 'u' || 
+    ch == 'A' || ch =='E' || ch == 'I' || ch == 'O' || ch == 'U')
+{
+System.out.println(" it is vowel ");
+}
+else 
+{
+System.out.println(" It is consonant ");
+}
+sc.close();
+}
+}
+
